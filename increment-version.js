b07ae7e2903ab2bcc2c69d5bcd08a74fef7bb5f4
roey-deeplan <roey@deeplan.co.il>
2024-01-01 15:11:51 +0200

    first commit

---
+++ increment-version.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const filePath = './config/package-solution.json'; // Update with actual JSON file path
+
+function incrementVersion(version) {
+    const parts = version.split('.').map(part => parseInt(part, 10));
+
+    for (let i = parts.length - 1; i >= 0; i--) {
+        if (parts[i] < 9) {
+            parts[i]++;
+            break;
+        } else {
+            if (i === 0) {
+                // If the first part is also 9, reset and add a new part at the beginning
+                parts[i] = 0;
+                parts.unshift(1);
+                break;
+            } else {
+                parts[i] = 0;
+            }
+        }
+    }
+
+    return parts.join('.');
+}
+
+function updateVersion() {
+    try {
+        const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        console.log("Current version:", json.solution.version);
+        json.solution.version = incrementVersion(json.solution.version);
+        fs.writeFileSync(filePath, JSON.stringify(json, json.solution.version, 2), 'utf8');
+        console.log(`Version updated to ${json.solution.version}`);
+    } catch (error) {
+        console.error("Error updating version:", error);
+    }
+}
+
+updateVersion();
---
+++ src/webparts/managementControl/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IManagementControlWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ManagementControlWebPartStrings' {
+  const strings: IManagementControlWebPartStrings;
+  export = strings;
+}
